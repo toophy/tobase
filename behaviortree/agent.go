d4401ed6ac3fea07d9e7997b81c255b09d1ed0bf
toophy@vip.qq.com <toophy@vip.qq.com>
2017-05-18 22:15:53 +0800

    增加json配置文件 增加树状堆栈(单独Prev,多个Next)

--- behaviortree/agent.go
+++ behaviortree/agent.go
@@ -54,22 +54,61 @@ func (a *Agent) SetValue(i int, v int64) {
 	}
 }
 
+// GetStack 获取当前行为树堆栈
+func (a *Agent) GetStack() *TreeStack {
+	return &a.Stacks
+}
+
 // TreeStack 行为树内存栈
 // 1. 行为树名称
 // 2. 执行的层数
-// 3. 第一层, 运行状态, 数组下标, 参数
-// 4. 第二层, 运行状态, 数组下标, 参数
+// 3. 第一层, 运行状态, 数组下标, 本层保存的数据(共调用的节点使用), 每种节点用了不同的数据, 只需要节点知道
+// 4. 第二层, 运行状态, 数组下标, 本层保存的数据(共调用的节点使用)
 // 5. ...
+// 6. 当并行的时候, 有可能一层有多个节点对象, 怎么处理这个并行呢?
+// 7. 栈也是做成树么?
+// 8. 每个栈都有一个Prev和Next, 用来区分层次, 而且可以有多个, 也就是有读个 Next, Prev只有一个
 type TreeStack struct {
-	Running  bool
-	MyAgent  *Agent
-	TreeName string
-	Layers   []TreeStackLayer
+	Running   bool
+	MyAgent   *Agent
+	TreeName  string
+	CurrLayer *TreeStackLayer
+	Layers    []*TreeStackLayer
+}
+
+// Push 压入一层栈
+func (t *TreeStack) Push(running bool, param interface{}) {
+	tsl := &TreeStackLayer{Running: running, Idx: len(t.Layers), Prev: t.CurrLayer, Params: param}
+	t.Layers = append(t.Layers, tsl)
+	// 压入之后要回调么?
+}
+
+// Pop 弹出一层
+func (t *TreeStack) Pop() {
+	if len(t.Layers) > 0 {
+		// 离开之前要回调么?
+		t.Layers = t.Layers[:len(t.Layers)-1]
+	}
+}
+
+// GetLayers 获取指定层堆栈
+func (t *TreeStack) GetLayers(idx int) *TreeStackLayer {
+	if idx >= 0 && idx < len(t.Layers) {
+		return t.Layers[idx]
+	}
+	return nil
+}
+
+// GetLayerSize 获取堆栈层数
+func (t *TreeStack) GetLayerSize() int {
+	return len(t.Layers)
 }
 
 // TreeStackLayer 行为树栈层
 type TreeStackLayer struct {
 	Running bool
 	Idx     int
-	Params  []interface{}
+	Prev    *TreeStackLayer   // 单独上层
+	Nexts   []*TreeStackLayer // 多个下层
+	Params  interface{}
 }
